967a89c977d580df8bc5ea4ccdc6dd0a3634e57e
RyuZhihao123 <liuzh96@outlook.com>
2020-04-16 03:01:44 +0800

    add PureExperiments

--- Codes_PureExperiment_default_5_times/Task2_clevelAndMcGill/3point_cloud_10/Net_IRNp.py
+++ Codes_PureExperiment_default_5_times/Task2_clevelAndMcGill/3point_cloud_10/Net_IRNp.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
 
             # to avoid stuck in local optimum at the beginning
             iter += 1
-            if iter >= 10 and iter < 15 and train_iter_loss > 0.05:
+            if iter >= 10 and iter < 15 and best_train_loss > 0.05:
                 history_iter.clear()
                 history_batch.clear()
                 best_train_loss = best_val_loss = 999999.
