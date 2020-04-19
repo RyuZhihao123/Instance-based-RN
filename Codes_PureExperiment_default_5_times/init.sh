c1ab1abc71f2aa2be67aeb04f07c23ad58d62ae3
RyuZhihao123 <liuzh96@outlook.com>
2020-04-20 03:22:06 +0800

    Update bash scripts

---
+++ Codes_PureExperiment_default_5_times/init.sh
@@ -0,0 +1,21 @@
+sed -i 's/\r//' run_all_cleveland.sh
+sed -i 's/\r//' run_all_ourNewTasks.sh
+
+
+sed -i 's/\r//' ./gpuCleveland/gpu1.sh
+sed -i 's/\r//' ./gpuCleveland/gpu2.sh
+sed -i 's/\r//' ./gpuCleveland/gpu3.sh
+sed -i 's/\r//' ./gpuCleveland/gpu4.sh
+sed -i 's/\r//' ./gpuCleveland/gpu5.sh
+sed -i 's/\r//' ./gpuCleveland/gpu6.sh
+sed -i 's/\r//' ./gpuCleveland/gpu7.sh
+sed -i 's/\r//' ./gpuCleveland/gpu8.sh
+
+sed -i 's/\r//' ./gpuOurNewTasks/gpu1.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu2.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu3.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu4.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu5.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu6.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu7.sh
+sed -i 's/\r//' ./gpuOurNewTasks/gpu8.sh
